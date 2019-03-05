726ffa30c73263b120f06f1ff7ca915ee952e97f
David Haynes <haynesd2@gmail.com>
2019-03-05 11:31:56 -0600

    added file inclas questions

---
+++ inclass_questions.sql
@@ -0,0 +1,73 @@
+
+/*
+Question 1
+For each geographic feature what is the average temperature in July 2005.
+    Census tract
+    County
+    State
+*/
+
+
+/*
+Question 2
+For each geographic unit, identify those that do not have any us cities
+    Census tract
+    County
+    State
+*/
+
+
+/*
+Question 3
+Calculate area for each geographic feature, what is the largest county in the continental United States?
+Calculate the difference between the attribute area and the calculated area.
+Calculate the population density people / per s/*
+Quare mile
+*/
+
+/*
+Question 4
+For this Question choose an interstate road. 
+For each geographic unit, identify the features that an the road goes through
+    Census tract
+    County
+*/
+
+/*
+Question 5
+For each geographic feature, find all adjacent geographic features to the state of Iowa which are not Iowa features.
+    Census tract
+    County
+*/
+
+/*
+Question 6
+For each geographic feature, identify which county has the most roads?
+    Census tract
+    County
+*/
+
+/*
+Question 7
+Identify which us cities are not within 30 kilometers of any road?
+Identify which us cities are within 5 kilometers of any interstate road.
+*/
+
+/*
+Question 8
+Choose a state for this analysis. 
+Identify for that particular state, which cities are furthest from the state centroid.
+*/
+
+/*
+Question 9
+What is the monthly average of cloudy days in Maine?
+What state has the highest average cloudy days for 2008?
+*/
+
+/*
+Question 10
+Choose your own time period.
+Identify the road segments that by county are the windiest?
+Identify the road segments that by county are the hottest?
+*/
